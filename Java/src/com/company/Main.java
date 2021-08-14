d18b3446827b8247b48264028bf4b348b81e9aaf
tmdrl5779 <tmdrl5779@gmail.com>
2021-08-14 23:39:58 +0900

    [ADD] 프로그래머스 소수 만들기, 음양 더하기, 체육복

--- Java/src/com/company/Main.java
+++ Java/src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import com.company.level_1.Weekly;
-import com.company.level_1.모의고사;
-import com.company.level_1.신규아이디추천;
-import com.company.level_1.크레인인형뽑기게임;
+import com.company.level_1.*;
 
 import java.util.*;
 
@@ -11,10 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] a = {1, 5, 3, 5, 1, 2, 1, 4};
-
-        int[] b = Arrays.copyOfRange(a, 2, 4);
-
-
     }
+
 }
---
+++ Java/src/com/company/level_1/체육복.java
@@ -0,0 +1,38 @@
+package com.company.level_1;
+
+import java.util.*;
+
+public class 체육복 {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        int[] countClothes = new int[n+2];
+
+        for(int lostPeople : lost)
+            countClothes[lostPeople]--;
+        for(int reservePeople : reserve)
+            countClothes[reservePeople]++;
+
+        for(int count : countClothes){
+            if(count == -1){
+                answer--;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(countClothes[i] == -1 && countClothes[i-1] == 1){
+                answer++;
+                countClothes[i]++;
+                countClothes[i-1]--;
+                continue;
+            }
+            if(countClothes[i] == -1 && countClothes[i+1] == 1){
+                answer++;
+                countClothes[i]++;
+                countClothes[i+1]--;
+                continue;
+            }
+        }
+
+        return answer;
+    }
+}
