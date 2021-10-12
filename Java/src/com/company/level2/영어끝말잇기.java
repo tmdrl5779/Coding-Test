280e3c31424d8fc7a77c133bd3706730a123ad56
tmdrl5779 <tmdrl5779@gmail.com>
2021-10-13 01:02:34 +0900

    [ADD] 프로그래머스 Level2

---
+++ Java/src/com/company/level2/영어끝말잇기.java
@@ -0,0 +1,42 @@
+package com.company.level2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 영어끝말잇기 {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+
+        int gameCount = 1;
+        int peopleCount = 0;
+        String prev = "";
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < words.length; i++){
+            String now = words[i];
+            peopleCount++;
+
+            if(i > 0){
+                //끝말 확인, 이미 존재하는 단어인지 확인
+                char prevChar = prev.charAt(prev.length()-1);
+                char nowChar = now.charAt(0);
+                if(prevChar != nowChar || map.containsKey(now)){
+                    answer[0] = peopleCount;
+                    answer[1] = gameCount;
+                    break;
+                }
+
+            }
+
+            map.put(now, 0);
+            prev = now;
+
+            if(peopleCount == n){
+                peopleCount = 0;
+                gameCount ++;
+            }
+        }
+
+        return answer;
+    }
+}
